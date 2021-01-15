92a10470d31d9125732122261018b98923f61ea8
Plofland <ploflan@gmail.com>
2021-01-14 19:04:02 -0500

    added some notes to the custom hook

--- src/components/hooks/useLocalStorage.js
+++ src/components/hooks/useLocalStorage.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 const useLocalStorage = (key, initialValue) => {
   const [storedValue, setStoredValue] = useState(() => {
+    //function needs to check if we have a value in localStorage already
     if (window.localStorage.getItem(key)) {
       return JSON.parse(localStorage.getItem(key));
     }
@@ -9,6 +10,7 @@ const useLocalStorage = (key, initialValue) => {
     return initialValue;
   });
 
+  //custom setter function that updates the state && sets the new value to localStorage
   const setValue = (value) => {
     window.localStorage.setItem(key, JSON.stringify(value));
     setStoredValue(value);
